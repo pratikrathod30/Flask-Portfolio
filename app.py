deb0641e0b0a98d1bac09a3d059e08ffac0a9371
pratikrathod30 <pratikrathod25555@gmail.com>
2025-11-24 17:09:28 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/contact", methods=["GET","POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        return f"Thanks {name}, your message is received!"
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
